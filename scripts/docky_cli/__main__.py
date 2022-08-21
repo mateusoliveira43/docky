ac30d2454777b5e4e68231ae40d2ffd655f4a8c4
Mateus Oliveira <matews1943@gmail.com>
2022-08-21 15:29:31 -0300

    feat: Add list, lint and sacn commands to Docky CLI

--- scripts/docky_cli/__main__.py
+++ scripts/docky_cli/__main__.py
@@ -5,7 +5,10 @@ from cly import config
 from . import __version__
 from .commands.down import down
 from .commands.env_file import env
+from .commands.lint import lint
+from .commands.list_container_related import list_container_related
 from .commands.run import run
+from .commands.scan import scan
 
 CLI_CONFIG = {
     "name": "Docky",
@@ -23,4 +26,8 @@ env_command = CLI.create_command(env)
 env_command.add_argument("-s", "--show", action="store_true")
 run_command = CLI.create_command(run)
 run_command.add_argument(dest="command", nargs="*")
+list_command = CLI.create_command(list_container_related, alias="ls")
+list_command.add_argument("-a", "--all", dest="show_all", action="store_true")
+CLI.create_command(lint)
+CLI.create_command(scan)
 CLI.create_command(down)
