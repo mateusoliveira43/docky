ccf97c17796ac97ec8c780d7a3b4a20ab01da047
Mateus Oliveira <matews1943@gmail.com>
2022-08-26 13:38:29 -0300

    fixup! feat: Add project documentation

--- scripts/docky_cli/__init__.py
+++ scripts/docky_cli/__init__.py
@@ -1,4 +1,4 @@
 """Docky Command Line Interface (CLI) package."""
 
 # https://github.com/mateusoliveira43/docky
-__version__ = "0.1.0"  # major.minor.patch
+__version__ = "1.0.0"  # major.minor.patch
