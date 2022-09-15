db703729d10ebd1726bbc8d2a7cd30fb8785fc73
Mateus Oliveira <matews1943@gmail.com>
2022-09-15 12:51:14 -0300

    fixup! feat: Add up command to CLI

--- instal_docky.py
+++ instal_docky.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 PROJECT_ROOT: Path = Path(__file__).resolve().parent
 DOCKY: str = "git@github.com:mateusoliveira43/docky.git"
+SOURCE: Path = PROJECT_ROOT / "docky/scripts"
 
 
 def get_docky() -> str:
@@ -32,10 +33,27 @@ def get_docky() -> str:
         return f"git clone {DOCKY}"
 
 
+def copy_files_to_scripts(folder: Path) -> None:
+    """
+    Copy files to the project's scripts folder.
+
+    Files with the same name are overwritten.
+
+    Parameters
+    ----------
+    folder : Path
+        Folder or file to be copied.
+
+    """
+    for file in folder.glob("*"):
+        if file.is_dir():
+            copy_files_to_scripts(file)
+        else:
+            destination = PROJECT_ROOT / "scripts" / file.relative_to(SOURCE)
+            destination.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(src=file, dst=destination)
+
+
 subprocess.run(get_docky(), shell=True, check=True, encoding="utf-8")  # nosec
-shutil.copytree(  # type: ignore
-    src=PROJECT_ROOT / "docky/scripts",
-    dst=PROJECT_ROOT / "scripts",
-    dirs_exist_ok=True,
-)
+copy_files_to_scripts(SOURCE)
 shutil.rmtree(PROJECT_ROOT / "docky")
